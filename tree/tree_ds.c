7a3c9ff99ccff342a6a2c61a4323c3235894b6eb
Dhruv Khanna <contact@rdciti.com>
2020-05-14 08:52:25 +0530

    added traversal

--- tree/tree_ds.c
+++ tree/tree_ds.c
@@ -7,12 +7,26 @@ struct node
 	struct node* left, * right;
 };
 
+struct stackNode
+{
+	struct stackNode* top;
+	struct stackNode* next;
+};
+
 void createtree(int);
 int search(int);
+
+void traverse();
+void push(struct node*);
+struct stackNode *pop();
 
 struct node* head = NULL;
 struct node* temp = NULL;
 struct node* prev = NULL;
+
+struct stackNode* q   = NULL;
+struct stackNode* top = NULL;
+struct stackNode* np  = NULL;
 
 int main()
 {
@@ -20,7 +34,7 @@ int main()
 
 	do
 	{
-		printf("enter your choice: \n 1. insert\n2. search\n3. exit\n");
+		printf("enter your choice: \n 1. insert\n2. search\n3. traverse\n4. exit\n");
 		scanf_s("%d", &choice);
 
 		switch (choice)
@@ -38,18 +52,24 @@ int main()
 
 			if (flag)
 			{
-				printf("\nnode FOUND in the tree/n");
+				printf("\nFOUND in the tree\n");
 			}
 			else
 			{
-				printf("\nnode NOT found\n");
+				printf("\nNOT found\n");
 			}
 
 		case 3:
+			printf("\n\nINORDER traversal: ");
+			traverse(head);
+
+		case 4:
+			printf("\n\nFreeing the memory\n\n");
+			head = NULL;
 			free(head);
 			break;
 		}
-	} while (choice != 3);
+	} while (choice != 4);
 
 	return 0;
 }
@@ -99,15 +119,16 @@ void createtree(int num)
 
 int search(int key)
 {
-	while (head != NULL)
+	temp = head;
+	while (temp != NULL)
 	{
-		if (key > head->data)
+		if (key > temp->data)
 		{
-			head = head->right;
+			temp = temp->right;
 		}
-		else if (key < head->data)
+		else if (key < temp->data)
 		{
-			head = head->left;
+			temp = temp->left;
 		}
 		else
 		{
@@ -115,4 +136,59 @@ int search(int key)
 		}
 	}
 	return 0;
+}
+
+void traverse()
+{
+	push(head);
+
+	temp = head;
+
+	while (top != NULL)
+	{
+		while (temp != NULL)
+		{
+			push(temp);
+			temp = temp->left;
+		}
+		if (top != NULL && temp == NULL)
+		{
+			temp = pop();
+			printf(" %d,", temp->data);
+			temp = temp->right;
+		}
+	}
+}
+
+void push(struct node *ptr)
+{
+	np = (struct stackNode*)malloc(sizeof(struct stackNode));
+	np->top = ptr;
+	np->next = NULL;
+	if (top == NULL)
+	{
+		top = np;
+	}
+	else
+	{
+		q = top;
+		top = np;
+		np->next = q;
+	}
+}
+
+struct stackNode *pop()
+{
+	if (top == NULL)
+	{
+		printf("underflow\n");
+	}
+	else
+	{
+		q = top;
+		top = top->next;
+		return(q->top);
+		q = NULL;
+		free(q);
+	}
 }
